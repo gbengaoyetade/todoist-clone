f1df38054acc1dcd0ad87d62c57547905847d8ab
gbengaPS <ioyetade@gmail.com>
2019-09-11 12:51:32 +0100

    create nav design

--- src/client/components/Commons/Nav.js
+++ src/client/components/Commons/Nav.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './commons.scss';
 
 const Nav = () => (
-  <nav className="app-nav">
-    <div className="nav-content">
-      <div className="inner-addon left-addon">
-        <i className="fas fa-search"></i>
-        <input type="text" className="form-control" />
+    <nav className="app-nav">
+      <div className="nav-content">
+        <div className="logo">
+          <Link to="/app">
+            <i className="fas fa-layer-group"></i>
+          </Link>
+        </div>
+        <div className="search-bar">
+          <i className="fas fa-search"></i>
+          <input type="text" className="search-input" />
+        </div>
+        <div className="nav-actions">
+          <div>
+            <i className="fas fa-plus"></i>
+          </div>
+          <div>
+            <i className="fas fa-star"></i>
+          </div>
+          <div>
+            <i className="fas fa-bell"></i>
+          </div>
+          <div>
+            <i className="fas fa-cog"></i>
+          </div>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
 );
 
 export default Nav;
